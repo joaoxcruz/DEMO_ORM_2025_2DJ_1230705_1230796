5a10d414af6d5406d5bc9c4be2237330d91e706d
Joáo Cruz <1230705@isep.ipp.pt>
2025-02-25 15:59:26 +0000

    Implementei o método main()

--- demo/src/main/java/isep/eapli/demo_orm/DemoORM.java
+++ demo/src/main/java/isep/eapli/demo_orm/DemoORM.java
@@ -1,7 +1,9 @@
 package isep.eapli.demo_orm;
 
+import isep.eapli.demo_orm.apresentacao.MainMenu;
+
 public class DemoORM {
     public static void main(String[] args) {
-
+        MainMenu.mainLoop();
     }
 }
