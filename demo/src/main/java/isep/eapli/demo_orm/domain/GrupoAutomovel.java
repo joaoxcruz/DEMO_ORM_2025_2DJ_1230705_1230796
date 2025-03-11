3d043b28c252bc8b4646392e6f64426c348024b1
Joáo Cruz <1230705@isep.ipp.pt>
2025-03-11 16:09:43 +0000

    Created files

--- demo/src/main/java/isep/eapli/demo_orm/domain/GrupoAutomovel.java
+++ demo/src/main/java/isep/eapli/demo_orm/domain/GrupoAutomovel.java
@@ -1,9 +1,14 @@
 package isep.eapli.demo_orm.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class GrupoAutomovel {
     private String classe;
     private int nPortas;
-
+    @Id
+    private Long id;
 
 
     public void alterarNumeroPortas(int nPortas){
@@ -16,5 +21,13 @@ public class GrupoAutomovel {
 
     public String toString(){
         return String.format("Classe = " + this.classe);
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
